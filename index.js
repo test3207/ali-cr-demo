150826c538946dfd4978bf1e8add8ee3eac6cd89
test3207 <test3207@outlook.com>
2020-01-15 16:14:17 +0800

    update

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ require('http')
 	.on('request', (req, res) => {
 		const { url } = req
 		const { exec } = require('child_process')
-		if (url.includes('cr')) exec('docker pull registry.cn-shanghai.aliyuncs.com/test3207/ali-cr-demo:master && docker run --name ali-cr-demo -p 7023:7023 --restart always -d registry.cn-shanghai.aliyuncs.com/test3207/ali-cr-demo:master', () => {})
+		if (url.includes('cr')) {
+			exec('docker pull registry.cn-shanghai.aliyuncs.com/test3207/ali-cr-demo:master && docker run --name ali-cr-demo -p 7023:7023 --restart always -d registry.cn-shanghai.aliyuncs.com/test3207/ali-cr-demo:master',
+				(err) => {
+					if (err && err.includes('name') && err.includes('is already in use')) {
+						exec('docker container restart blogs')
+					}
+				})
+		}
 		res.writeHead(200)
 		res.end(`
 			<html>Hello World!</html>
